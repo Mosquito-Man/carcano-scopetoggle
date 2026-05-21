d2341eaf9951e96d33bb8b043739364f7aae2705
Mosquito-Man <awesato@gmail.com>
2026-05-21 04:57:15 +0300

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+game "rdr3"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+author "Mosquito"
+
+description "Adds the ability to toggle the scope on the Carcano sniper rifle."
+
+file 'weaponinfo_main.meta'
+
+data_file 'WEAPONINFO_FILE_PATCH' 'weaponinfo_main.meta'
